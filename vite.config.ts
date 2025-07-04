cc1ddaf19e1f3848f77733b52435053b4851b4d7
renard <derrenardl@google.com>
2025-07-04 19:15:45 +0400

    фывафываф

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-homepage-react/',  // <-- название твоего репозитория на github
+  base: 'my-homepage-react/',  // <-- название твоего репозитория на github
   plugins: [react()],
 });
